6456b11dcaaee5aec436da73e4e5302f0be203e8
initial commit <>
2019-07-29 16:52:01 +0300

    Remove vue dep from main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue';
-import App from './App.vue';
+// import Vue from 'vue';
+// import App from './App.vue';
 
 import Tabs from './components/Tabs.vue';
 import Tab from './components/Tab.vue';
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app');
 
 const install = (VueLib) => {
   VueLib.component(Tabs.name, Tabs);
